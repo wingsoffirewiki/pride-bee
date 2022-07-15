63f3af76e4fdb95c0b5dbe33fed17b4bb791f98b
Evan <evanrayweigel@outlook.com>
2022-07-14 23:34:17 -0500

    update flags command

--- src/commands/flags.ts
+++ src/commands/flags.ts
@@ -1,12 +1,50 @@
 import * as Discord from "discord.js";
 import { Command } from "fero-dc";
-import { flagsSorted } from "../util/flags";
+import { flagsSorted, getFlagImage } from "../util/flags";
 
 export default new Command()
   .name("flags")
   .description("Get the currently available flags")
   .category("Utility")
-  .run((client, interaction) => {
+  .options([
+    {
+      name: "flag",
+      description: "The flag to view",
+      type: "STRING",
+      required: false
+    }
+  ])
+  .run(async (client, interaction) => {
+    const flagString = interaction.options.getString("flag", false);
+
+    if (flagString) {
+      const flag = await getFlagImage(flagString);
+
+      if (!flag) {
+        interaction.reply({
+          ephemeral: true,
+          content: `Flag \`${flagString}\` not found`
+        });
+
+        return;
+      }
+
+      const embed = new Discord.MessageEmbed();
+      const attachment = new Discord.MessageAttachment(flag, "flag.png");
+
+      embed
+        .setTitle(`Flag \`${flagString}\``)
+        .setImage("attachment://flag.png")
+        .setColor("RANDOM");
+
+      interaction.reply({
+        embeds: [embed],
+        files: [attachment]
+      });
+
+      return;
+    }
+
     const embed = new Discord.MessageEmbed();
 
     const flagEntries = Object.entries(flagsSorted);
@@ -19,7 +57,7 @@ export default new Command()
       .setDescription(
         `Here are all **${
           flags.length
-        }** flags I currently support!\nIf you want to suggest/add a flag, please use /contribute.\n\n${flags.join(
+        }** flags I currently support!\nDo /flags [flag] to view the flag.\nIf you want to suggest/add a flag, please use /contribute.\n\n${flags.join(
           "\n"
         )}`
       )
