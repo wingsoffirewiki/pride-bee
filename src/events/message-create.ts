1842eaeceaa5dc5b2018e61276037d1051687327
Evan <evanrayweigel@outlook.com>
2022-07-15 13:54:41 -0500

    don't delete the messages

--- src/events/message-create.ts
+++ src/events/message-create.ts
@@ -1,21 +1,9 @@
 import { Event } from "fero-dc";
-import { setTimeout as sleep } from "node:timers/promises";
 
 export default new Event<"messageCreate">()
   .event("messageCreate")
-  .run(async (client, message) => {
+  .run((client, message) => {
     if (message.content.startsWith("b!")) {
-      const reply = await message.reply(
-        "Pride Bee has been converted to slash commands"
-      );
-
-      await sleep(5000);
-
-      try {
-        reply.delete();
-        message.delete();
-      } catch {
-        console.log("Bot does not have delete perms");
-      }
+      message.reply("Pride Bee has been converted to slash commands");
     }
   });
