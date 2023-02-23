278421d51a4442104b0a5d3560c0b6f789961607
Evan <evanrayweigel@outlook.com>
2023-02-23 17:52:53 -0600

    Delete message-create.ts

--- src/events/message-create.ts
+++
@@ -1,9 +0,0 @@
-import { EventListener } from "fero-dc";
-
-export default new EventListener<"messageCreate">()
-  .setEvent("messageCreate")
-  .setHandler((client, message) => {
-    if (message.content.startsWith("b!")) {
-      message.reply("Pride Bee has been converted to slash commands");
-    }
-  });
