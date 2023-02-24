19c7b126a269afb6ee31b1b2c62795e1e4233b6d
Evan <evanrayweigel@outlook.com>
2023-02-23 19:00:48 -0600

    refactor ready.ts

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,61 +1,41 @@
 import { ActivityType } from "discord.js";
 import { Client, EventListener } from "fero-dc";
-import fs from "fs";
+import * as fs from "fs";
 import { flagsSorted } from "../util/flags";
-import prettier from "prettier";
+
+const HOUR_IN_MILLISECONDS = 60 * 60 * 1000;
 
 export default new EventListener<"ready">()
   .setEvent("ready")
   .setHandler(async (client) => {
     setPresence(client);
-    sortImageList();
-    checkIfEachImageExists();
+    setInterval(() => setPresence(client), HOUR_IN_MILLISECONDS);
+
+    if (!imagesExist()) {
+      console.log("❌ Some flags do not exist.");
+      process.exit(1);
+    }
   });
 
 function setPresence(client: Client<true>): void {
-  const presence = () =>
-    client.user.setPresence({
-      status: "online",
-      afk: false,
-      activities: [
-        {
-          name: `/pride in ${client.guilds.cache.size} servers.`,
-          type: ActivityType.Watching
-        }
-      ]
-    });
-
-  presence();
-  setInterval(presence, 60000);
+  client.user.setPresence({
+    status: "online",
+    activities: [
+      {
+        name: `/pride in ${client.guilds.cache.size} servers.`,
+        type: ActivityType.Watching
+      }
+    ]
+  });
 }
 
-function sortImageList(): void {
-  fs.writeFileSync(
-    "./src/config/flags.json",
-    prettier.format(JSON.stringify(flagsSorted), {
-      parser: "json"
-    })
-  );
-}
+function imagesExist(): boolean {
+  const flags = Object.keys(flagsSorted);
+  return flags.every((flag) => {
+    const exists = fs.existsSync(`./assets/flags/${flag}.png`);
 
-function checkIfEachImageExists(): void {
-  const allExist = Object.keys(flagsSorted)
-    .map((flag) => {
-      const exists = fs.existsSync(`./assets/flags/${flag}.png`);
+    console.assert(exists, `❌ ${flag}'s image not found.`);
 
-      console.log(
-        exists ? `✅ ${flag}'s image found.` : `❌ ${flag}'s image not found.`
-      );
-
-      return exists;
-    })
-    .every((exists) => exists);
-
-  if (allExist) {
-    console.log("✅ All images found.");
-  } else {
-    console.log("❌ Some flags do not exist.");
-
-    process.exit(1);
-  }
+    return exists;
+  });
 }
