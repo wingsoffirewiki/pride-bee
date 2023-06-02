96032af352eb327953b74beff7f228a528e59a15
Evan <evanrayweigel@outlook.com>
2023-06-01 20:23:05 -0500

    hopefully fix something

--- src/util/casing.ts
+++ src/util/casing.ts
@@ -1,4 +1,8 @@
 export function toPascalCase(str: string): string {
+	if (str.length === 0) {
+		return str;
+	}
+
 	return str
 		.split(/[- A-Z]/)
 		.map((word) => word[0].toUpperCase() + word.substring(1))
