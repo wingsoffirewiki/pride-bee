2ef26ba68ec6f3b7234d573512f62da92a4a5812
Evan <evanrayweigel@outlook.com>
2023-06-01 19:44:17 -0500

    remove imagesExist function

--- src/listeners/on-ready.ts
+++ src/listeners/on-ready.ts
@@ -1,8 +1,6 @@
 import { Client, EventListener } from "@ferod/client";
 import chalk from "chalk";
 import { ActivityType, Events } from "discord.js";
-import * as fs from "fs";
-import { flags } from "../util/flags";
 
 const HOUR_IN_MILLISECONDS = 60 * 60 * 1000;
 
@@ -13,10 +11,6 @@ export default new EventListener()
 
 		setPresence(client);
 		setInterval(() => setPresence(client), HOUR_IN_MILLISECONDS);
-
-		if (!imagesExist()) {
-			process.exit(1);
-		}
 	});
 
 function setPresence(client: Client<true>): void {
@@ -28,15 +22,5 @@ function setPresence(client: Client<true>): void {
 				type: ActivityType.Watching,
 			},
 		],
-	});
-}
-
-function imagesExist(): boolean {
-	return flags.every((flag) => {
-		const exists = fs.existsSync(`./assets/flags/${flag}.png`);
-
-		console.assert(exists, `❌ ${flag}'s image not found.`);
-
-		return exists;
 	});
 }
